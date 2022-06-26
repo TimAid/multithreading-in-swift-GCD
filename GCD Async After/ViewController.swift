c67be0d118a116d57d0914bd87c8c07882199338
Timaid <vip.firemen@icloud.com>
2022-06-26 22:13:41 +0800

    multithreading in swift GCd Async After

---
+++ GCD Async After/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCD Async After
+//
+//  Created by Aid on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
